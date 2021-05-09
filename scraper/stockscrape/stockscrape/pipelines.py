add6599c185afcd77890aa2192442c881d378842
Piotr <pplebank@gmail.com>
2021-05-09 19:26:46 -0400

    added pipeline and connection to db

--- scraper/stockscrape/stockscrape/pipelines.py
+++ scraper/stockscrape/stockscrape/pipelines.py
@@ -6,8 +6,31 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import psycopg2
+import sys, os
+import numpy as np
+import pandas as pd
+from stockscrape.credentials import Credentials as creds
+import pandas.io.sql as psql
 
 
 class StockscrapePipeline:
+
+    def open_spider(self, spider):
+        try:
+            connect_string = "host="+ creds.HOST +" port="+ "5432" +" dbname="+ creds.PGDATABASE +" user=" + creds.PGUSER +" password="+ creds.PGPASSWORD
+            self.connect=psycopg2.connect(conn_string)
+            self.cursor = connect.cursor()
+
+        except(Exception,psycopg2.Error) as error:
+            print("Failed to insert record into mobile table", error)
+
     def process_item(self, item, spider):
+        postgres_sql_command = """INSERT INTO stock_links(stock_name,stock_link,stock_last_update) VALUES (%s,%s,%s)"""
+        item_to_add = (item.get('name'),item.get('URL'),item.get('date'))
+        self.cursor.execute(postgres_sql_command,item_to_add)
         return item
+
+    def close_spider(self, spider):
+        self.connect.close()
+        self.connect.cursor()
--- scraper/stockscrape/stockscrape/spiders/moneypl.py
+++ scraper/stockscrape/stockscrape/spiders/moneypl.py
@@ -1,6 +1,6 @@
 import scrapy
 import re
-
+from datetime import date
 
 class MoneyplSpider(scrapy.Spider):
     name = 'moneypl'
@@ -13,17 +13,26 @@ class MoneyplSpider(scrapy.Spider):
         stocks = table.xpath(".//div[contains(@class,'rt-tr-group')]")
 
         for stock in stocks:
+            name_stock = stock.xpath(".//a[contains(@class,'sc-18yizqs-0 sUail')]/div/text()").get()
+
             link_stock = stock.xpath(".//a[contains(@class,'sc-18yizqs-0 sUail')]/@href").get()
             url_stock = self.modify_url(link_stock)
             absolute_url = response.urljoin(url_stock)
+
+            date_today = '10.05.2021'
+
             yield {
-                'URL' : absolute_url
+                'name' : name_stock,
+                'URL' : absolute_url,
+                'date' : date_today
             }
             #yield Request(url_stock, callback=self.parse_single_stock)
 
     def modify_url(self,extracted_link):
-        modified_url = re.sub('^/gielda/spolki-gpw/','',extracted_link) 
-        modified_url = re.sub('\.html$','',modified_url)  
-        modified_url += ',finanse.html'
-        return modified_url  
+        modified_url = '';
+        if extracted_link != None :
+            modified_url = re.sub('^/gielda/spolki-gpw/','',extracted_link) 
+            modified_url = re.sub('\.html$','',modified_url)  
+            modified_url += ',finanse.html'
+        return modified_url 
 
